11723b25888481e4f4724aa377768d0d1fcc4d9a
JosuaMnl <josuamnalu@gmail.com>
2022-11-28 15:52:12 +0700

    Menghilangkan status bar dan action bar

--- app/src/main/java/com/yosha10/splashscreen/SplashActivity.java
+++ app/src/main/java/com/yosha10/splashscreen/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide(); // Menghilangkan Action Bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // Menghilangkan Status Bar
 
         new Handler().postDelayed(new Runnable() {
             @Override
